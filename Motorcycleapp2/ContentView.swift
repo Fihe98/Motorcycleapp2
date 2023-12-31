f24c047dadd6d96aea95fb080b2866a7690d4490
FiHe98 <henriksson.filip1998@gmail.com>
2023-12-31 15:42:02 +0100

    Initial Commit

---
+++ Motorcycleapp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Motorcycleapp2
+//
+//  Created by Filip Henriksson on 2023-12-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Motorcycleapp2/Motorcycleapp2App.swift
@@ -0,0 +1,17 @@
+//
+//  Motorcycleapp2App.swift
+//  Motorcycleapp2
+//
+//  Created by Filip Henriksson on 2023-12-31.
+//
+
+import SwiftUI
+
+@main
+struct Motorcycleapp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
