1e1e507b787f889d0d06c913ea4f1f67553ecef2
FiHe98 <henriksson.filip1998@gmail.com>
2024-01-20 21:20:13 +0100

    Improved MotorcycleDetailView UI

---
+++ Motorcycleapp2/CompareSheet.swift
@@ -0,0 +1,18 @@
+//
+//  CompareSheet.swift
+//  Motorcycleapp2
+//
+//  Created by Filip Henriksson on 2024-01-12.
+//
+
+import SwiftUI
+
+struct CompareSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompareSheet()
+}
